f6d32f69c0ce9b19ab0b981cff1d3c001d94170d
019201 <456_mxx_wwp>
2018-09-26 13:37:45 +0800

    no message

--- activityServiceImpl.java
+++ activityServiceImpl.java
@@ -1,5 +1,8 @@
+
+add func1
 @Override
 public void onBackPressed() {
+add func2
     Intent home = new Intent(Intent.ACTION_MAIN);
     home.addCategory(Intent.CATEGORY_HOME);
     startActivity(home);
